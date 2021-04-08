d2ca68cd25d6af3dc3bb166e8a25f10a6bc345b1
assaf <assafpetronio@gmail.com>
2021-04-08 21:33:02 +0300

    added formik functionallity

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -4,22 +4,19 @@ import { DarkTheme, MainBlue, MainGreen } from "../Styles/colors"
 import { BoxSize, LeafIcon } from "../Styles/styles"
 import {Flex, FormControl, FormErrorMessage, FormLabel} from '@chakra-ui/react';
 import LeafLogo from '../Assets/images/leaf-green.png';
-import { useState } from "react"
 import { Link } from "react-router-dom"
-import { useContext } from "react"
-import Store from "../Context/global/global-context"
 import {Formik, Form, Field} from 'formik';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const {_, dispatch} = useContext(Store);
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const {_, dispatch} = useContext(Store);
 
-    const handleLogin = async (e) => {
-        const data = { email:email, passsword:password};
-        const demoToken = '23123412432534645grvgdfgeg234cvdfsbdhdsty'
-        dispatch({type:'USER_LOGIN', payload: demoToken})
-    }
+    // const handleLogin = async (e) => {
+    //     const data = { email:email, passsword:password};
+    //     const demoToken = '23123412432534645grvgdfgeg234cvdfsbdhdsty'
+    //     dispatch({type:'USER_LOGIN', payload: demoToken})
+    // }
 
 
     return (
@@ -29,29 +26,38 @@ const Login = () => {
                 <Flex>
                     <BoxSize flexSize="1" style={{padding:'1.5rem', background:'transparent'}} >
 
-                    <Formik>
-                        {(props) => (
-                            <Form>
-                            <Field name="email">
-                                {({ field, form }) => (
-                                <FormControl id="email" isRequired>
-                                    <FormLabel color='white' fontSize="1.1rem" textAlign="left" pb="2" >Email address</FormLabel>
-                                    <Input type="email" border="1px solid white" bg={DarkTheme} mb="5" onChange={(e) => setEmail(e.target.value)} />
-                                    <FormErrorMessage>'</FormErrorMessage>
-                                </FormControl>
-                                )}
-                            </Field>
+                    <Formik
+                        initialValues={{email:'', password:''}}
+                        onSubmit={(data, {setSubmitting}) => {
+                            setSubmitting(true);
+                            //make async call
+                            console.log('submit: ', data);
+                            setSubmitting(false)
+                        } }
+                    >
+                        {({values, isSubmitting, handleChange, handleBlur, handleSubmit}) => (
+                            <Form onSubmit={handleSubmit}>
 
-                            <Field name="email">
-                                {({ field, form }) => (
-                                <FormControl id="password" isRequired>
-                                    <FormLabel color='white' fontSize="1.1rem" textAlign="left" pb="2" >Password</FormLabel>
-                                    <Input type="password" border="1px solid white" bg={DarkTheme} mb="5"onChange={(e) => setPassword(e.target.value)}  />
-                                    <FormErrorMessage>'</FormErrorMessage>
-                                </FormControl>
-                                )}
-                            </Field>
-                            <Button type="submit" w="100%" bg={MainGreen} color="white" colorScheme="green" onClick={handleLogin}>Lets roll</Button>
+                                <Field>
+                                    {({ field, form }) => (
+                                    <FormControl id="email" isRequired >
+                                        <FormLabel color='white' fontSize="1.1rem" textAlign="left" pb="2" >Email address</FormLabel>
+                                        <Input name="email" value={values.email} onChange={handleChange} onBlur={handleBlur}  border="1px solid white" bg={DarkTheme} mb="5" />
+                                        <FormErrorMessage>'</FormErrorMessage>
+                                    </FormControl>
+                                    )}
+                                </Field>
+
+                                <Field>
+                                    {({ field, form }) => (
+                                    <FormControl id="password" isRequired>
+                                        <FormLabel color='white' fontSize="1.1rem" textAlign="left" pb="2" >Password</FormLabel>
+                                        <Input type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} border="1px solid white" bg={DarkTheme} mb="5" />
+                                        <FormErrorMessage>'</FormErrorMessage>
+                                    </FormControl>
+                                    )}
+                                </Field>
+                                <Button disabled={isSubmitting} type="submit" w="100%" bg={MainGreen} color="white" colorScheme="green">Lets roll</Button>
                             </Form>
                         )}
                     </Formik>
--- src/Pages/sign-up.js
+++ src/Pages/sign-up.js
@@ -1,33 +1,29 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { Button } from "@chakra-ui/button";
 import { Flex } from "@chakra-ui/layout";
-import { useState } from "react";
 import { DarkTheme,  MainGreen } from "../Styles/colors";
 import { BoxSize, LeafIcon } from "../Styles/styles";
 import LeafLogo from '../Assets/images/leaf-green.png';
 import { Input } from "@chakra-ui/input";
 import { Link } from "react-router-dom";
-import useCheckEmailValid from "../Utils/useCheckEmailValid";
 import { FormControl, FormErrorMessage, FormHelperText, FormLabel } from "@chakra-ui/form-control";
 import {Formik, Form, Field} from 'formik';
 
 const Signup = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
 
-    const handleSignUp = async () => {
-        if(password === confirmPassword){
-            if(useCheckEmailValid(email)){
-                const data = { email:email, passsword:password};
-                console.log('post request', data);
-            } else {
-                console.log('email not valid')
-            }
-        } else {
-            console.log('passwords not matching')
-        }
-    }
+
+    // const handleSignUp = async () => {
+    //     if(password === confirmPassword){
+    //         if(useCheckEmailValid(email)){
+    //             const data = { email:email, passsword:password};
+    //             console.log('post request', data);
+    //         } else {
+    //             console.log('email not valid')
+    //         }
+    //     } else {
+    //         console.log('passwords not matching')
+    //     }
+    // }
 
     return (
         <Flex Flex justify='center' align="center" m="auto" w="fit-content" h="93vh">
@@ -35,43 +31,51 @@ const Signup = () => {
                     <LeafIcon src={LeafLogo} />
                     <Flex>
                         <BoxSize flexSize="1" style={{padding:'1.5rem', background:'transparent'}}>
-                            <Formik>
-                            {(props) => (
+                            <Formik
+                                initialValues={{email:'', password:'', confirmPassword:''}}
+                                onSubmit={(data, {setSubmitting}) => {
+                                    setSubmitting(true);
+                                    //make async call
+                                    console.log('submit: ', data);
+                                    setSubmitting(false)
+                                }}
+                            >
+                        {({values, isSubmitting, handleChange, handleBlur, handleSubmit}) => (
                                 <Form>
-                                <Field name="email">
+                                <Field>
                                     {({ field, form }) => (
                                         <FormControl id="email" isRequired>
                                             <FormLabel color='white' fontSize="1.1rem" textAlign="left" pb="2" >Email address</FormLabel>
-                                            <Input type="email" border="1px solid white" bg={DarkTheme}  onChange={(e) => setEmail(e.target.value)}/>
+                                            <Input type="email" name="email" value={values.email} onChange={handleChange} onBlur={handleBlur} border="1px solid white" bg={DarkTheme} />
                                             <FormHelperText mb="5">We'll never share your email.</FormHelperText>
                                             <FormErrorMessage>'</FormErrorMessage>
                                         </FormControl>
                                     )}
                                 </Field>
 
-                                <Field name="password">
+                                <Field>
                                     {({ field, form }) => (
                                         <FormControl id="password" isRequired>
                                             <FormLabel color='white' fontSize="1.1rem" textAlign="left" pb="2" >Password</FormLabel>
-                                            <Input type="password" border="1px solid white" bg={DarkTheme} mb="5" onChange={(e) => setPassword(e.target.value)}  />
+                                            <Input type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} border="1px solid white" bg={DarkTheme} mb="5" />
                                             <FormErrorMessage>'</FormErrorMessage>
                                         </FormControl>
                                     )}
                                 </Field>
 
 
-                                <Field name="confirmPassword">
+                                <Field>
                                     {({ field, form }) => (
                                         <FormControl id="confirm-password" isRequired >
                                             <FormLabel color='white' fontSize="1.1rem" textAlign="left" pb="2" >Confirm password</FormLabel>
-                                            <Input  type="password" border="1px solid white" bg={DarkTheme} onChange={(e) => setConfirmPassword(e.target.value)}  />
+                                            <Input type="password" name="confirm-password" value={values.confirmPassword} onChange={handleChange} onBlur={handleBlur} border="1px solid white" bg={DarkTheme} />
                                             <FormHelperText mb="5">Please match the passwords</FormHelperText>
                                             <FormErrorMessage>'</FormErrorMessage>
                                         </FormControl>
                                     )}
                                 </Field>
                                 <Link to="/signup/questions">
-                                    <Button type="submit" w="100%" bg={MainGreen} color="white" colorScheme="green" onClick={handleSignUp}>Lets roll</Button>
+                                    <Button disabled={isSubmitting} type="submit" w="100%" bg={MainGreen} color="white" colorScheme="green">Lets roll</Button>
                                 </Link>
                             </Form>
                             )}
